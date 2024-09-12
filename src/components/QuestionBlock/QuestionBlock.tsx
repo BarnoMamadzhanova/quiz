f30e9474e348c95717019ca1722e8f8a7a3d98a1
BarnoMamadzhanova <b@mamadzhanova.com>
2024-09-12 13:26:19 +0200

    creating components structure, d.ts file to declare module.css

---
+++ src/components/QuestionBlock/QuestionBlock.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./QuestionBlock.module.css";
+
+function QuestionBlock() {
+  return <div></div>;
+}
+
+export default QuestionBlock;
---
+++ src/components/TabsBlock/TabsBlock.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./TabsBlock.module.css";
+
+function TabsBlock() {
+  return <div></div>;
+}
+
+export default TabsBlock;
